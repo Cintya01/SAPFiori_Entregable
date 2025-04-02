319871456ce231ecfc24225276e9fd34247f4537
Cintya Olivares Cisternas <cintya.olivares.c@gmail.com>
2025-04-02 05:14:05 +0000

    falta filtro

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,17 +6,27 @@ sap.ui.define([
 
     return Controller.extend("com.bootcamp.sapui5.finalproject.controller.Home", {
         onInit() {
+            
             this._loadData();
+            
             
         },
 
         async _loadData() {
             try {
-                let oDatos = await HomeHelper.getDataSuppliers();
+                let oDatos = await HomeHelper.getDataSuppliers(this.getView().getModel());
                 await HomeHelper.setSuppliersModel(this, oDatos[0].results);
             } catch (error) {
                 console.error("Error al obtener los datos: ", error);
             }
+        },
+
+        onInputSearch: function () {
+            HomeHelper.onSearch(this);
+        },
+
+        onCountrySearch: function(){
+            HomeHelper.onSearch(this);
         }
         
     });
--- webapp/utils/HomeHelper.js
+++ webapp/utils/HomeHelper.js
@@ -16,43 +16,51 @@ sap.ui.define([
                 return HomeService.readSuppliers(this._oNorthwindModel, oFilters);
             },
 
-            setSuppliersModel: async function (oController, aDatos) {
-                let oListModel = oController.getOwnerComponent().getModel('SuppliersCollection');
-                 if(!oListModel){
-                 const oModel  = new JSONModel([]);
-                 oModel.setSizeLimit(1000000);	
-                 oController.getOwnerComponent().setModel(oModel, "SuppliersCollection");  
-                 oListModel = oController.getOwnerComponent().getModel('SuppliersCollection');
-             }
- 
+
+            onSearch: async function (controller) {
+                let aFilters = [];
+                let sIdOrName= controller.byId("idOrNameInput").getValue();
+                //filtro id o nombre
+                if(sIdOrName) {
+                    aFilters.push(new Filter({
+                        filters: [
+                            new Filter("SupplierID", FilterOperator.Contains, sIdOrName),
+                            new Filter("ContactName", FilterOperator.Contains, sIdOrName),
+                        ],
+    
+                    }));
+                }
+                //filtro dropdown pais
+                let sCountry = controller.byId("countrySelect").getSelectedKey();
+                if (sCountry) {
+                    aFilters.push(new Filter("Country", FilterOperator.Contains, sCountry))
+                }
+    
+                try{
+                    const aSuppliers = await HomeService.readSuppliers(this._oNorthwindModel, aFilters);
+                    this.setSuppliersModel(this, aSuppliers);
+                }catch{
+                    console.error("error en data filtro")
+                }
+             },
+    
+
+
+
+             setSuppliersModel: async function (controller, aDatos) {
+                let oListModel = controller.getView().getModel('SuppliersCollection');
+                if (!oListModel) {
+                    const oModel = new JSONModel([]);
+                    oModel.setSizeLimit(1000000);
+                    controller.getView().setModel(oModel, "SuppliersCollection");
+                    oListModel = controller.getView().getModel('SuppliersCollection');
+                }
+
              oListModel.setData(aDatos);
          }, 
 
-         onSearch: async function () {
-            let aFilters = [];
-            let sIdOrName= this.byId("idOrNameInput").getValue();
-            //filtro id o nombre
-            if(sIdOrName) {
-                aFilters.push(new Filter({
-                    filters: [
-                        new Filter("SupplierID", FilterOperator.Contains, sIdOrName),
-                        new Filter("ContactName", FilterOperator.Contains, sIdOrName),
-                    ],
+         
 
-                }));
-            }
-            //filtro dropdown pais
-            let sCountry = this.byId("countrySelect").getSelectedKey();
-            if (sCountry) {
-                aFilters.push(new Filter("Country", FilterOperator.Contains, sCountry))
-            }
-
-            try{
-
-            }catch{
-                
-            }
-         }
             
     
     
